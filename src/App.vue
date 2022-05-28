a9938bae7b7b1a05f3f8f1ec4cdff3e7169edb1f
student24 <student24@pcoip.ki.agh.edu.pl>
2022-05-28 10:13:12 +0200

    Add alert button, add validate for form

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <h1>Mail to: {{email}}</h1>
+    <div class = "validate" v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div class = "validate" v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div class = "validate" v-else>Twój adres e-mail jest stanowczo za długi.</div>
     <input type="email" v-model="email">
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
   </div>
 </template>
 
@@ -11,7 +15,14 @@ export default {
     return {
       email: ' '
     };
-  }
+  },
+
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    }
+}
+
 }
 
 </script>
@@ -24,5 +35,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.validate {
+  color: red;
 }
 </style>
