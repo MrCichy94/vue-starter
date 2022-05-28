2f817e87798feae6630ed2b1702282aab5c3bb95
student24 <student24@pcoip.ki.agh.edu.pl>
2022-05-28 09:54:03 +0200

    Add reactive element

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Mail to: {{email}}</h1>
+    <input type="email" v-model="email">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      email: ' '
+    };
   }
 }
+
 </script>
 
 <style>
