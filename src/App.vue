6f01d3b6d604915eb8d7942d4a9a96e77dc36b5d
MrCichy94 <student24@pcoip.ki.agh.edu.pl>
2022-05-28 11:19:08 +0200

    Finish first part project

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,37 @@
 <template>
   <div id="app">
-    <h1>Mail to: {{email}}</h1>
-    <div class = "validate" v-if="email.length < 10">Ale masz krótki adres!</div>
-    <div class = "validate" v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div class = "validate" v-else>Twój adres e-mail jest stanowczo za długi.</div>
-    <input type="email" v-model="email">
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+    <div v-if="!isAuthenticated">
+      <h1>WITAJ W EKRANIE LOGOWANIA</h1>
+      <h1>Mail to: {{email}}</h1>
+      <div class = "validate" v-if="email.length < 10">Ale masz krótki adres!</div>
+      <div class = "validate" v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+      <div class = "validate" v-else>Twój adres e-mail jest stanowczo za długi.</div>
+      <input type="email" v-model="email" @keyup.enter = "logIn()">
+      <button @click="logIn()">Zaloguj</button>
+    </div>
+    <div v-else>
+      <h1>Witaj na zajęciach {{email}}!</h1>
+      <button @click="logOut()">Wyloguj</button>
+    </div>
   </div>
 </template>
 
 <script>
+import "milligram";
 export default {
   data() {
     return {
-      email: ' '
+      email: ' ',
+      isAuthenticated: false
     };
   },
 
   methods: {
-    alertMyEmail() {
-      alert(this.email);
+    logIn() {
+      this.isAuthenticated = true;
+    },
+    logOut() {
+      this.isAuthenticated = false;
     }
 }
 
